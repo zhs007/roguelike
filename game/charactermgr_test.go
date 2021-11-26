55f121f34ecfeeb9f3f92dbd508e315b6f15d764
zhs007 <sssxueren@gmail.com>
2021-11-26 08:48:08 +0000

    Updated test

--- game/charactermgr_test.go
+++ game/charactermgr_test.go
@@ -9,12 +9,15 @@ import (
 func Test_LoadCharacterMgr(t *testing.T) {
 	mgr, err := LoadCharacterMgr("../gamedata/character.xlsx")
 	assert.NoError(t, err)
-	assert.Equal(t, len(mgr.MapCharacters), 2)
+	assert.Equal(t, len(mgr.MapCharacters), 41)
 
 	assert.Equal(t, mgr.MapCharacters[1].ID, 1)
-	assert.Equal(t, mgr.MapCharacters[1].Name, "主角")
-	assert.Equal(t, mgr.MapCharacters[1].HP, 100)
-	assert.Equal(t, mgr.MapCharacters[1].Attack, 30)
+	assert.Equal(t, mgr.MapCharacters[1].Name, "绿头怪")
+	assert.Equal(t, mgr.MapCharacters[1].HP, 50)
+	assert.Equal(t, mgr.MapCharacters[1].Attack, 20)
+	assert.Equal(t, mgr.MapCharacters[1].Defence, 1)
+	assert.Equal(t, mgr.MapCharacters[1].Gold, 1)
+	assert.Equal(t, mgr.MapCharacters[1].Exp, 1)
 
 	t.Logf("Test_LoadCharacterMgr OK")
 }
